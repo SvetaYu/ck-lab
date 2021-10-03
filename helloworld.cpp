0540a037fd4e001fb44642af785d0a3755ee05d6
Andrew Mikhno <w.mikhno@gmail.com>
2021-10-03 17:06:54 +0300

    new line

--- helloworld.cpp
+++ helloworld.cpp
@@ -7,5 +7,7 @@ int main()
     cout << "Hello world!";
     cout << ' ' << "BB laba";
     cout << ' ' << "I want a good mark";
+    cout << ' ' << "Sveta bolshaya umnitsa!";
+    cout << endl << "Probuem pull request";
     return 0;
 }
